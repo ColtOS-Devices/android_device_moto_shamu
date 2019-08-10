f4eb113e7209c81ce678bfd503ace9a2afc5b07a
nitin1438 <nitin.chobhe@gmail.com>
2019-08-10 20:49:39 +0530

    Add maintainer name

--- colt_shamu.mk
+++ colt_shamu.mk
@@ -44,3 +44,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="shamu-user 7.1.1 N6F27M 4299435 release-keys"
 
 BUILD_FINGERPRINT := google/shamu/shamu:7.1.1/N6F27M/4299435:user/release-keys
+
+# Maintainer Prop
+PRODUCT_PROPERTY_OVERRIDES += \
+ 	ro.colt.maintainer=Nitin1438
